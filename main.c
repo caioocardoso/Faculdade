30fde2217a015a89debfb9d065e042934ff7913b
caioocardoso <136840071+caioocardoso@users.noreply.github.com>
2024-03-15 15:06:58 -0300

    q02

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int base, altura, area, perimetro;
-    printf("Escreva abaixo o valor da base do retângulo");
-    scanf("%i", &base);
-    printf("Escreva abaixo o valor da altura do retângulo");
-    scanf("%i", &altura);
+    int lado, area, perimetro;
+    printf("Escreva abaixo o valor do lado do quadrado");
+    scanf("%i", &lado);
     
-    area = base * altura;
-    perimetro = 2*base + 2*altura;
+    area = lado*lado;
+    perimetro = 4*lado;
     
     printf ("O valor da área é %i e o perimetro é %i", area, perimetro);
     return 0;
