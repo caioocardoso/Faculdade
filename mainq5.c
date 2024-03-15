9222fb86e18eed9d612f0fc8842dea1c17b2098c
caioocardoso <136840071+caioocardoso@users.noreply.github.com>
2024-03-15 15:10:06 -0300

    Create mainq5.c

---
+++ mainq5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int numero, sucessor;
+    printf("Escreva");
+    scanf("%i", &numero);
+    
+    sucessor = numero + 1;
+    
+    printf ("O sucessor é %i", sucessor);
+    return 0;
+}
